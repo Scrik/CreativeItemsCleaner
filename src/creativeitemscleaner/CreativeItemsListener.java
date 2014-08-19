891882b1722b381c8c9b38a9c3a3e4ad9ec77965
Shevchik <Shev4ik.den@gmail.com>
2014-08-19 19:20:24 +0400

    Split to classes

--- src/creativeitemscleaner/CreativeItemsListener.java
+++ src/creativeitemscleaner/CreativeItemsListener.java
@@ -17,19 +17,11 @@
 
 package creativeitemscleaner;
 
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.EnchantmentStorageMeta;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class CreativeItemsListener implements Listener {
 
@@ -40,50 +32,8 @@ public class CreativeItemsListener implements Listener {
 		}
 		ItemStack item = event.getCursor();
 		if ((item != null) && (item.getType() != Material.AIR)) {
-			event.setCursor(generateCleanItem(item));
+			event.setCursor(ItemStackCleaner.generateCleanItem(item));
 		}
-	}
-
-	public ItemStack generateCleanItem(ItemStack oldItem) {
-		//handle base
-		ItemStack newItem = new ItemStack(oldItem.getType());
-		//handle possibly invalid count (never seen this, but who knows what can possibly happen?)
-		if (oldItem.getAmount() > oldItem.getMaxStackSize()) {
-			newItem.setAmount(oldItem.getMaxStackSize());
-		} else {
-			newItem.setAmount(oldItem.getAmount());
-		}
-		newItem.setDurability(oldItem.getDurability());
-		//handle books
-		if (oldItem.getType() == Material.ENCHANTED_BOOK) {
-			//handle enchants
-			if (oldItem.hasItemMeta()) {
-				EnchantmentStorageMeta oldEnchBookMeta = (EnchantmentStorageMeta) oldItem.getItemMeta();
-				if (oldEnchBookMeta.hasStoredEnchants()) {
-					//just add the first found enchants if it is valid
-					Map<Enchantment, Integer> enchants = oldEnchBookMeta.getStoredEnchants();
-					if (enchants.size() > 0) {
-						Entry<Enchantment, Integer> entry = enchants.entrySet().iterator().next();
-						if (entry.getValue() <= entry.getKey().getMaxLevel()) {
-							EnchantmentStorageMeta newEnchBookMeta = (EnchantmentStorageMeta) Bukkit.getItemFactory().getItemMeta(Material.ENCHANTED_BOOK);
-							newEnchBookMeta.addStoredEnchant(entry.getKey(), entry.getValue(), false);
-							newItem.setItemMeta(newEnchBookMeta);
-						}
-					}
-				}
-			}
-			//now handle name
-			ItemMeta newItemMeta = null;
-			if (newItem.hasItemMeta()) {
-				newItemMeta = newItem.getItemMeta();
-			} else {
-				newItemMeta = Bukkit.getItemFactory().getItemMeta(Material.ENCHANTED_BOOK);
-			}
-			newItemMeta.setDisplayName(ChatColor.YELLOW + "Enchanted Book");
-			newItem.setItemMeta(newItemMeta);
-		}
-		//return newly generated item
-		return newItem;
 	}
 
 }
